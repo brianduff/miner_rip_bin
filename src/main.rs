df7df6c062908ed696f960660e1d61791e5a43f4
Brian Duff <cairndubh@gmail.com>
2023-07-08 01:24:04 -0700

    Write out sprites

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{Seek, SeekFrom, Read}};
+use std::{fs::File, io::{Seek, SeekFrom, Read, BufWriter}, path::Path};
 
 use color::SpectrumColor;
 use anyhow::Result;
@@ -11,15 +11,66 @@ fn main() -> Result<()> {
     file.seek(SeekFrom::Start(0xb000))?;
 
     let mut caverns = Vec::with_capacity(20);
-
     for _ in 0..20 {
         file.read_exact(&mut buf)?;
-        caverns.push(Cavern::try_from(&buf[..])?);
+        let cavern = Cavern::try_from(&buf[..])?;
+        caverns.push(cavern);
     }
 
     println!("Loaded {} caverns", caverns.len());
 
+    let sprites: Vec<_> = caverns.iter().flat_map(|c| &c.tile_sprites).collect();
+    export_sprites(sprites, "/tmp/newsprites.png")?;
+
     Ok(())
+}
+
+fn export_sprites<P: AsRef<Path>>(sprites: Vec<&Sprite>, path: P) -> Result<()> {
+    let columns = 16;
+    let file = File::create(path)?;
+    let w = &mut BufWriter::new(file);
+
+    println!("Got {} sprites with {} columns", sprites.len(), columns);
+
+    // Fixme - hardcoded assumption that eash sprite is 8x8
+    let width = 8 * columns;
+    let rows = (sprites.len() / columns) + 1;
+    let height  = 8 * rows;
+
+    let mut encoder = png::Encoder::new(w, width as u32, height as u32);
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header()?;
+
+    let mut merged = Vec::with_capacity(width * height * 4);
+
+    for pixel_row in 0..height {
+        for pixel_col in 0..width {
+            let block_col = pixel_col / 8;
+            let block_row = pixel_row / 8;
+            let sprite_num = (block_row * columns) + block_col;
+
+            let sprite_row = pixel_row % 8;
+            let sprite_col = pixel_col % 8;
+
+            if sprite_num < sprites.len() {
+                //println!("[{},{},{}]", sprite_num, sprite_row, sprite_col);
+                merged.push(sprites[sprite_num].to_rgba()[sprite_row][sprite_col*4]);
+                merged.push(sprites[sprite_num].to_rgba()[sprite_row][sprite_col*4 + 1]);
+                merged.push(sprites[sprite_num].to_rgba()[sprite_row][sprite_col*4 + 2]);
+                merged.push(sprites[sprite_num].to_rgba()[sprite_row][sprite_col*4 + 3]);
+            }
+        }
+    }
+
+    let expected_count = width * height * 4;
+
+    merged.resize(expected_count, 0);
+
+    writer.write_image_data(&merged)?;
+
+    Ok(())
+
 }
 
 // A cavern
@@ -102,20 +153,29 @@ impl Sprite {
         Ok(Sprite{ pixel_width, pixel_height, bytes, color })
     }
 
-    /// Converts this sprite into rgba data.
-    fn to_rgba(&self) -> Vec<u8> {
-        let mut result = Vec::with_capacity(32 * self.bytes.len());
-        for byte in self.bytes.iter() {
-            let mut mask: u8 = 0b10000000;
-            for _ in 0..8 {
-                result.append(&mut if byte & mask != 0 {
-                    self.color.ink_rgba()
-                } else {
-                    self.color.paper_rgba()
-                });
-                mask >>= 1;
+    /// Converts this sprite into 2d rgba data.
+    fn to_rgba(&self) -> Vec<Vec<u8>> {
+        let mut result = Vec::with_capacity(self.pixel_height);
+        let mut byte_index = 0;
+
+        let mut byte_iter = self.bytes.iter();
+        for pixel_row in 0..self.pixel_height {
+            let mut cols: Vec<u8> = Vec::with_capacity(self.pixel_width);
+            for b in 0..self.pixel_width / 8 {
+                let byte = byte_iter.next().unwrap();
+                let mut mask: u8 = 0b10000000;
+                for _ in 0..8 {
+                    cols.append(&mut if byte & mask != 0 {
+                        self.color.ink_rgba()
+                    } else {
+                        self.color.paper_rgba()
+                    });
+                    mask >>= 1;
+                }
             }
+            result.push(cols);
         }
+
         result
     }
 }
